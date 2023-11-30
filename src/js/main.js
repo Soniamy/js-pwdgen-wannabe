a46c5ba689b59e7b02ee4bdd4a7f035309447b43
Soniamy <marianna.marzullo@gmail.com>
2023-11-30 16:13:20 +0100

    fine esercizio

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
-console.log('ciao')
-const name = prompt("Qual'è il tuo nome?")
-const surname = prompt("Qual'è il tuo cognome?")
-const color = prompt("Qual'è il tuo colore preferito?")
+let name = prompt("Qual'è il tuo nome?")
+let surname = prompt("Qual'è il tuo cognome?")
+let color = prompt("Qual'è il tuo colore preferito?")
+let resulte = name + surname + color + 114
+document.getElementById('resulte').innerHTML = resulte
+console.log(name + surname + color + 114)
